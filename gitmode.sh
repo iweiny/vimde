829c3baf7d29aa18f0f6c2706e6d782aacfdad01
Ira Weiny <ira.weiny@intel.com>
2016-09-29 19:21:10 -0400

    vimde: implement --git mode
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- gitmode.sh
+++ gitmode.sh
@@ -7,11 +7,15 @@ vimde_home=${HOME}/.vimde
 
 log_opts="--pretty=oneline --decorate --abbrev-commit -n 200"
 
-# find the head of the branch we are possibly rebasing
-head=`git branch | grep "*" | grep rebasing | sed -e "s/.* rebasing \(.*\))/\1/"`
+head=$2
+
+if [ $head == "" ]; then
+	# find the head of the branch we are possibly rebasing
+	head=`git branch | grep "*" | grep rebasing | sed -e "s/.* rebasing \(.*\))/\1/"`
+fi
 
 pushd $1
-git log $log_opts $head | vim -S $vimde_home/vimde.git.cmds -n -R -
+git log $log_opts $head | vim -s ./vimde/s.gvimrc -S $vimde_home/vimde.git.cmds -n -R -
 popd
 
 exit 0
